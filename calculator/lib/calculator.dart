02b53b0531b44cf8ddfe98c99e10b5468f4fe1be
Nathan Imogo <nathan.imogo@outlook.fr>
2024-03-27 15:19:01 +0100

    modified:   calculator/lib/calculator.dart

--- calculator/lib/calculator.dart
+++ calculator/lib/calculator.dart
@@ -56,16 +56,6 @@ class _CalculatorState extends State<Calculator> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              ' $_counter + 2 = ',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            const Text(
-              'You have pushed the button this many times:',
-            ),
             Text(
               ' $_counter + 2 = ${_counter + 2} fois!',
               style: Theme.of(context).textTheme.headlineMedium,
